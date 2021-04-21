50ba0bfaeadb08f6115a2b3dc22d09b6ae8aa1a2
zlincon <linconsilva08@gmail.com>
2021-04-20 22:49:10 -0300

    Aula 2 concluded

---
+++ lib/inmana.ex
@@ -0,0 +1,12 @@
+defmodule Inmana do
+  @moduledoc """
+  Inmana keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
+  alias Inmana.Restaurant.Create
+
+  defdelegate create_restaurant(params), to: Create, as: :call
+end
